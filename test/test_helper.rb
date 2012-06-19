017f5fabca54d96c94c14b9ab357187d725fbe1b
David van Geest <david.vangeest@gmail.com>
2012-06-19 18:26:08 -0400

    Tests run.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,20 @@
-ENV["RAILS_ENV"] = "test"
+require 'bundler'
+Bundler.require
 
-# For Generators
+require 'test/unit'
+require 'rails/all'
+require 'rails/generators'
 require 'rails/generators/test_case'
+
+class TestApp < Rails::Application
+  config.root = File.dirname(__FILE__)
+end
+Rails.application = TestApp
+
+module Rails
+  def self.root
+    @root ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'tmp', 'rails'))
+  end
+end
+Rails.application.config.root = Rails.root
+
